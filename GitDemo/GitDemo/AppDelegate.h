e83a23dca7d0f3af3d0e4cc6784af12b82d63b18
岩哥思密达 <ZCY@yangesimidadeMacBook-Pro.local>
2016-04-01 14:50:42 +0800

    第一次创建项目

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by 岩哥思密达 on 16/4/1.
+//  Copyright © 2016年 张超岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by 岩哥思密达 on 16/4/1.
+//  Copyright © 2016年 张超岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
