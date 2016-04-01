86e4434ad9243283cc621c2e57db3abe1c6e65f8
岩哥思密达 <ZCY@yangesimidadeMacBook-Pro.local>
2016-04-01 15:06:14 +0800

    B创建一个PersonB类

---
+++ GitDemo/GitDemo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GitDemo
+//
+//  Created by 岩哥思密达 on 16/4/1.
+//  Copyright © 2016年 张超岩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
